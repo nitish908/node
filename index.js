c16913a882994183f19b62e27d1ef969c278d97a
nitish908 <86035306+nitish908@users.noreply.github.com>
2023-03-27 08:42:09 +0530

    commit

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const express = require('express');
+'use strict'
 
-const app = express();
+var express = require('../../');
 
-app.get('/', function (req, res){
-    res.send("Hello World 2");
+var app = module.exports = express()
+
+app.get('/', function(req, res){
+  res.send('Hello World');
 });
 
-app.listen(80);
+/* istanbul ignore next */
+if (!module.parent) {
+  app.listen(3000);
+  console.log('Express started on port 3000');
+}
